48dc6d6d35bdd47228722a6bb226058f7e6a2ef3
dmitriitotskii <dimatocky@gmail.com>
2026-02-21 01:50:16 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
         site: 'https://Q2Key.github.io',
-        base: '/dmt-blog',
+        base: '/dmblogue',
         integrations: [mdx(), sitemap()],
 });
